4ad27a8c57ce594ea5eaf35b379005a6646a3057
hardip441 <chauhanhardip441@gmail.com>
2025-12-24 09:38:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -151,35 +151,59 @@ async def start(client, message):
         )
         return
     data = message.command[1]
-    if data.split("-", 1)[0] == "VJ":
-        user_id = int(data.split("-", 1)[1])
-        vj = await referal_add_user(user_id, message.from_user.id)
-        if vj and PREMIUM_AND_REFERAL_MODE == True:
-            await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
-            num_referrals = await get_referal_users_count(user_id)
-            await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
-            if num_referrals == REFERAL_COUNT:
-                time = REFERAL_PRE
-                buttons = [[
-                    InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                    ],[
-                    InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
-                    InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                    ],[
-                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('ᴇᴀʀɴ ᴍᴏɴᴇʏ', callback_data="shortlink_info")
-                    ],[
-                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ ᴀɴᴅ ʀᴇғᴇʀʀᴀʟ', callback_data='subscription')
-                    ]]
-        time = REFERAL_PREMEIUM_TIME       
-                seconds = await get_seconds(time)
-                if seconds > 0:
-                    expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
-                    user_data = {"id": user_id, "expiry_time": expiry_time} 
-                    await db.update_user(user_data)  # Use the update_user method to update or insert user data
-                    await delete_all_referal_users(user_id)
-                    await client.send_message(chat_id = user_id, text = "<b>You Have Successfully Completed Total Referal.\n\nYou Added In Premium For {}</b>".format(REFERAL_PREMEIUM_TIME))
-                    return 
+if data.split("-", 1)[0] == "VJ":
+    user_id = int(data.split("-", 1)[1])
+    vj = await referal_add_user(user_id, message.from_user.id)
+
+    if vj and PREMIUM_AND_REFERAL_MODE == True:
+        await message.reply(
+            f"<b>You have joined using the referral link of user with ID {user_id}\n\n"
+            "Send /start again to use the bot</b>"
+        )
+
+        num_referrals = await get_referal_users_count(user_id)
+
+        await client.send_message(
+            chat_id=user_id,
+            text="<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(
+                message.from_user.mention, num_referrals
+            )
+        )
+
+        if num_referrals == REFERAL_COUNT:
+            time = REFERAL_PREMEIUM_TIME
+
+            buttons = [[
+                InlineKeyboardButton(
+                    '⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬',
+                    url=f'http://t.me/{temp.U_NAME}?startgroup=true'
+                )
+            ],[
+                InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
+                InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
+            ],[
+                InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
+                InlineKeyboardButton('ᴇᴀʀɴ ᴍᴏɴᴇʏ', callback_data="shortlink_info")
+            ],[
+                InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ ᴀɴᴅ ʀᴇғᴇʀʀᴀʟ', callback_data='subscription')
+            ]]
+
+            seconds = await get_seconds(time)
+            if seconds > 0:
+                expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+                user_data = {"id": user_id, "expiry_time": expiry_time}
+
+                await db.update_user(user_data)
+                await delete_all_referal_users(user_id)
+
+                await client.send_message(
+                    chat_id=user_id,
+                    text=(
+                        "<b>You Have Successfully Completed Total Referal.\n\n"
+                        f"You Added In Premium For {REFERAL_PREMEIUM_TIME}</b>"
+                    )
+                )
+            return
         else:
     if PREMIUM_AND_REFERAL_MODE == True:
         buttons = [[
@@ -1396,6 +1420,7 @@ async def purge_requests(client, message):
             disable_web_page_preview=True
         )
         
+
 
 
 
